8547b67134d9c3d07dc93c61510da0fc3dd8fc68
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-06-01 16:52:22 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public/assets/js');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+module.exports =  {
+  entry: APP_DIR + '/index.js',
+  output: {
+    publicPath: '/assets/js/',
+    path: BUILD_DIR,
+    filename: 'bundle.min.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.js?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
+};
